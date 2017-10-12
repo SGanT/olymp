88d2d329630cbb8601ac373d89ffff1c4e28bd68
SGanT <sgant1@mail.ru>
2017-10-12 23:07:16 +0300

    Delete pointer.py

--- Домашка/Расставить точки в длинном числе/pointer.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def pointer(string):
-    string = str(string)[::-1]
-    splitted = re.split("(\d\d\d)",  string)
-    final = [x for x in splitted if x is not '']
-    return ".".join(final)[::-1]
-
-print pointer( raw_input("Your number ->") )
